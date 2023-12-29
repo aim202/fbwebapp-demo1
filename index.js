58bc8525cbfdd64e4dcb9bd0270829e263a000b0
aimeg2023 <aimeg2023@penguin>
2023-12-28 17:44:03 -0800

    From local-source

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNVXiROnbPayqggL-bRZ9QB_9Vak1o1zc",
+  authDomain: "simple-website-407104.firebaseapp.com",
+  projectId: "simple-website-407104",
+  storageBucket: "simple-website-407104.appspot.com",
+  messagingSenderId: "48357170688",
+  appId: "1:48357170688:web:80378148c0b5fea1e1c2e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
